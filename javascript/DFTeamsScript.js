4e7f3e2ac2d8e27507358ed394cd2f50e78f7414
ext.tiago.vieira <ext.tiago.vieira@farfetch.com>
2021-12-18 18:40:44 +0000

    implement archive team concept

--- javascript/DFTeamsScript.js
+++ javascript/DFTeamsScript.js
@@ -2,6 +2,37 @@ getTeams();
 
 var teams;
 
+setTimeout(function() {
+  addBtnListeners();
+}, 1000);
+
+function addBtnListeners() {
+  var allarchiveButtons = document.querySelectorAll('.archiveBtn');
+
+  console.log("length: " + allarchiveButtons.length);
+  for (var i = 0; i < allarchiveButtons.length; i++) {
+    allarchiveButtons[i].addEventListener('click', function() {
+      if (archiveConfirmation(this)) {
+        var teamId = getBtnId(this).substr(4);
+        archiveTeamAPI(teamId);
+      }
+    });
+  }
+}
+
+function archiveConfirmation(Btn) {
+  var retVal = confirm("Do you want to continue ?");
+  return retVal;
+}
+
+function getBtnId(elt) {
+  // Traverse up until root hit or DIV with ID found
+  while (elt && (elt.tagName != "TR" || !elt.id))
+    elt = elt.parentNode;
+  if (elt) // Check we found a DIV with an ID
+    return elt.id;
+}
+
 function getTeams() {
   fetch("http://"+API_URL+"/api/betstrat/drawfiboseq/teams")
     .then(function(response) {
@@ -38,7 +69,8 @@ function getTeams() {
 function addTeamToTable(idTeam, name, numMatches, balance, admin, oddAvg, season) {
   $(document).ready(function() {
     $('#teamsTable').append(
-      '<tr style="height: 64px;"><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"><a style="color: black;" href="Matches-By-Team.html?'+idTeam+'&'+name+'"><u>' + name + '</u></a></td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"><label class="switch"><input id="' + idTeam + '" onclick="toggleButton(this);"  type="checkbox" ' + admin + '><span class="slider round"></span></label></td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + numMatches + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + season + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + oddAvg + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + balance + '</td></tr>'
+      '<tr id="' + idTeam + '" style="height: 64px;"><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"><a style="color: black;" href="Matches-By-Team.html?'+idTeam+'&'+name+'"><u>' + name + '</u></a></td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"><label class="switch"><input id="' + idTeam + '" onclick="toggleButton(this);"  type="checkbox" ' + admin + '><span class="slider round"></span></label></td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + numMatches + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + season + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + oddAvg + '</td><td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + balance + '</td>' +
+      '<td class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"> <form><input class="archiveBtn" type=button value="📜" style="max-width:80%; position: center;"></form> </td></tr>'
     );
   });
 }
@@ -66,6 +98,25 @@ function toggleConfirmation(tgBtn) {
 
 function updateTeamAPI(teamId, admin) {
   var url = "http://" + API_URL + "/api/betstrat/drawfiboseq/" + teamId + "?admin=" + admin;
+
+  fetch(url, {
+      method: 'PUT', // or 'PUT'
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status) {
+        alert(data.error + "\n" + data.message);
+      } else {
+        console.log(data);
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+function archiveTeamAPI(teamId) {
+  var url = "http://" + API_URL + "/api/betstrat/drawfiboseq/team/archive" + teamId;
 
   fetch(url, {
       method: 'PUT', // or 'PUT'
--- javascript/constants.js
+++ javascript/constants.js
@@ -1,1 +1,2 @@
 var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
+//var API_URL= "localhost:8080";
