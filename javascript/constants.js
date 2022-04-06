6611757f8ce12b70ef8ab9a63d7d7f30a69338a5
Tiago Vieira <tiago.vieira@farfetch.com>
2022-04-06 18:02:40 +0100

    fix constants.js

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,5 +1,5 @@
- //var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
- var API_URL= "20.101.56.205:8880";
+ var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
+ //var API_URL= "20.101.56.205:8880";
 //var API_URL= "localhost:8080";
 var EH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSMl0iAf9AFkdJIYrz2rAwqzumP9U3cMwNLT6LdqkiEn2A94SL2jtdLmyN59h59DcG57FHftYqFDgS/pubhtml";
 var OD_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3oegceHoFnXIXMIdgk16r0uIdaLaLo0fHRTUHunhTN2AxJdLZ2n__Q58Sam5EIaRhe_gsW0YBdua/pubhtml";
