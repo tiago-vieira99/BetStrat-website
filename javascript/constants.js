b8ccc5327a4370dcd9c841ad2adb258e4c680857
Tiago Vieira <tiago.vieira@farfetch.com>
2022-07-06 16:15:18 +0100

    add new stratPath constants

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,8 +1,10 @@
  var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
- //var API_URL= "54.172.210.130:8880";
+ //const API_URL= "54.172.210.130:8880";
 //var API_URL= "localhost:8080";
-var EH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSMl0iAf9AFkdJIYrz2rAwqzumP9U3cMwNLT6LdqkiEn2A94SL2jtdLmyN59h59DcG57FHftYqFDgS/pubhtml";
-var OD_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3oegceHoFnXIXMIdgk16r0uIdaLaLo0fHRTUHunhTN2AxJdLZ2n__Q58Sam5EIaRhe_gsW0YBdua/pubhtml";
-var ONLY_DRAWS_ID = 1;
-var EURO_HANDICAP_ID = 2;
+const EH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSMl0iAf9AFkdJIYrz2rAwqzumP9U3cMwNLT6LdqkiEn2A94SL2jtdLmyN59h59DcG57FHftYqFDgS/pubhtml";
+const OD_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3oegceHoFnXIXMIdgk16r0uIdaLaLo0fHRTUHunhTN2AxJdLZ2n__Q58Sam5EIaRhe_gsW0YBdua/pubhtml";
+const ONLY_DRAWS_ID = 1;
+const EURO_HANDICAP_ID = 2;
+const ONLY_DRAWS_PATH = "onlydraws";
+const EURO_HANDICAP_PATH = "eurohandicap";
 
